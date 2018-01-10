813bdf8e5a9d74582052ea156feb0233b81b8f0b
Mikkel Ridley <mikkel250@gmail.com>
2018-01-10 12:46:55 -0800

    Added solution for repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-var repeatString = function() {
-
+var repeatString = function(string, multiple) {
+	let stringArray = [];
+	if (multiple < 0) {
+		return "ERROR";
+	}
+	for (let i = 0; i < multiple; i++) {
+		stringArray.push(string);
+	}
+	return stringArray.join("");
 }
 
 module.exports = repeatString
