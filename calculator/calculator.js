b04e318716c853de22eb6b4aa6aa10d9ebfcad6c
Mikkel Ridley <mikkel250@gmail.com>
2018-01-10 15:42:29 -0800

    Add changes to removeFromArray and calculator (both still need work)

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,43 @@
-function add () {
-	
+function add (a, b) {
+	let result = a + b;
+	return result;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	let result = a - b;
+	return result;
 }
 
-function sum () {
-	
+function sum (array) {
+	let result = 0;
+	for (let i = 0; i < array.length; i++) {
+		result = result + array[i];
+	}
+	return result;
 }
 
 function multiply () {
-	
+	var result = 1;
+	for (let i = 0; i < arguments.length; i++) {
+		
+		result = result * arguments[i];
+		}
+		return result;
 }
 
-function power() {
-	
+function power(a, b) {
+	let result = a ** b;
+	return result;
 }
 
-function factorial() {
-	
+function factorial(n) {
+	if (n < 0) {
+		return -1;
+	} 	else if (n == 0) {
+			return 1;
+			} else {
+				return (n * factorial(n - 1));	
+				}
 }
 
 module.exports = {
--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 var removeFromArray = function() {
+	// check arguments to see if they include an array. 
+	// Another approach would be to assume that only argument[0] is an array
+	let array = arguments[0];
+	// if the arguments are not an array, then use those arguments and unshift from argument 1
 
+	// going to probably want to loop backwards through the arguments.
+	for (i = arguments.length; i > arguments[0]; i--) {
+		// if an item is not found within the array, keep looping. TODO
+		if (arguments[i] != array[0...array.length -1]) {
+			continue;
+		}
+	}
 }
 
 module.exports = removeFromArray
