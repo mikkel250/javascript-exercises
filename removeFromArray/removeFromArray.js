b77f563687422d1d5cae8002b43eba3dd4c0d6df
Mikkel Ridley <mikkel250@gmail.com>
2018-01-11 16:48:00 -0800

    updated removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -2,15 +2,17 @@ var removeFromArray = function() {
 	// check arguments to see if they include an array. 
 	// Another approach would be to assume that only argument[0] is an array
 	let array = arguments[0];
+	let argsToRemoveFromArray = Array.prototype.slice.call(arguments, 0);
 	// if the arguments are not an array, then use those arguments and unshift from argument 1
 
 	// going to probably want to loop backwards through the arguments.
 	for (i = arguments.length; i > arguments[0]; i--) {
-		// if an item is not found within the array, keep looping. TODO
-		if (arguments[i] != array[0...array.length -1]) {
-			continue;
+		// pull the value out of the array, and pull the arugument, compare values
+		if (array[i] == argsToRemoveFromArray[i]) {
+			array.splice(i, 1);
 		}
 	}
+	return array;
 }
 
 module.exports = removeFromArray
