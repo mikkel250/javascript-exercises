af65c544002af7e08a0e5344d698cab330c3952f
Mikkel Ridley <mikkel250@gmail.com>
2018-01-12 12:05:42 -0800

    Added solution to tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-var ftoc = function() {
-  
+var ftoc = function(tempInF) {
+  let result =(tempInF - 32) * 5 / 9;
+  return Math.round(10 * result)/10;
 }
 
-var ctof = function() {
-  
+var ctof = function(tempInC) {
+  let result = (tempInC * 9 / 5) + 32;
+  return Math.round(10 * result)/10;
 }
 
 module.exports = {
