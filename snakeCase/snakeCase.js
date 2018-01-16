aa7b4e1f78bbd34f3160bca03476b0e0ca312232
Mikkel Ridley <mikkel250@gmail.com>
2018-01-15 20:26:59 -0800

    Worked on snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,8 @@
-var snakeCase = function() {
-
+var snakeCase = function(str) {
+  var replaceWith = / .\-!@#$%^&*/gi;
+  var newString = str.toLowerCase();
+  var newString = str.replace(replaceWith, `_`);
+  return newString;
 }
 
 module.exports = snakeCase
