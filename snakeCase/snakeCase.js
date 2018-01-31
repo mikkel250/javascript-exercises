2884ba680ae00756e55c54b0090e6816a5480ad2
Mikkel Ridley <mikkel250@gmail.com>
2018-01-30 21:32:20 -0800

    worked on snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,7 +1,9 @@
 var snakeCase = function(str) {
-  var replaceWith = / .\-!@#$%^&*/gi;
-  var newString = str.toLowerCase();
-  var newString = str.replace(replaceWith, `_`);
+  let newString = str.toLowerCase();
+  	newString.match(/[^_\W]+/g).join(`_`);
+  //var replaceWith = / .\-!@#$%^&*/gi;
+  //var newString = str.toLowerCase();
+  //var newString = str.replace(replaceWith, `_`);
   return newString;
 }
 
