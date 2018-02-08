d0213f71be7c4cd95449a35e431371e62a8769ef
mikkel250 <mikkel250@gmail.com>
2018-02-07 16:34:54 -0800

    nearly finished caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,8 +1,8 @@
 var caesar = function(string, shift) {
-	var letters = string.split("");
-	letters.map(charCodeAt)
-		if (letters[i].)
-	}
+	return string
+								.split("")
+								.map(char => shiftCharacter(char, shift) )
+								.join("");	
 }
 
 // Check the case of the letter
@@ -16,51 +16,23 @@ function upperOrLowerCase (char) {
 
 // Get the character code
 function getCharCode (char) {
-
+	return char.charCodeAt();
 }
 
 // Shift the character 
 function shiftCharacter (char, shift) {
-	let code = getCharCode(char);
-	let case = upperOrLowerCase(char);
-	return (code + shift % 26) + case;
+	let code = getCharCode(char);  // get the ascii code for the character
+	let charCase = upperOrLowerCase(char); 	// determine upper or lower case, and set 'a' or 'A' as the starting point for the shift
+	if ( (code > 64 && code < 91) || (code > 96 && code < 123) ) {    // if A-Z or a-z
+	let shiftedChar = (code + shift - charCase) % 26 + charCase;  // wrap around alphabet if necessary (the % gets the amount of shift, starting from the letter 'a', and charCase starts the count from 'a'). If the shift is past the last letter, it starts the count from 'a'. If it doesn't wrap around, you're getting the same number you would get if you added the charCode and shift together anyway. 
+	return String.fromCharCode(shiftedChar);	// return the string corresponding to the number code
+	} else {
+		return char; 	// otherwise, just return the character without modifying it
+	}
+
 }
 
 
 module.exports = caesar
-
-
-/* Initial thoughts:
-Create an object with each letter of the alphabet tied to a number
-
-Split the user input by character, then loop through that array character by character
-Check that character against the object, get the index, and then push that index number 
-	+/- user input number into a new array. Use findIndex() for this (array cardio day2)
-
-Will have to use some sort of logic to tell the program that if the index is less than 0, start counting
-	backwards from the end of the object and vice versa with the last number in the object. (e.g. -4 starts
-	at index -1, +27 starts at index 0). 
-
-
-Things to definitely use:
-charCodeAt()
-Ascii character codes  http://www.asciitable.com/
-0=48
-9=57
-A=65
-Z=90
-a=97 
-z=122
-
-http://www.seth-dehaan.com/2016/01/21/freecodecamp-caesar-s-cipher/
-
-Remember to split the alphabet in 2, so first half would be + the number and second half would be - the number
-You'll also have to write one loop for upper and one for lower case. 
-
-
-Things to consider using
-Map function?
-
-Sort the array?
 
 
