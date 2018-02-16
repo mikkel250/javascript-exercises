fcf6f70a759baa2e8432c9481a12f53687402b4d
mikkel250 <mikkel250@gmail.com>
2018-02-15 16:52:30 -0800

    did some work, needs revamp

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -3,15 +3,37 @@ const twoCharConsonantClusters = ["bl", "br", "ch", "cl", "cr", "dr", "fl", "fr"
 const threeCharConsonantClusters = ["sch", "scr", "shr", "spl", "spr", "squ", "str", "thr"];
 
 function translate(userInput) {
+	var firstTwo = userInput.slice(0, 3);
+	var firstThree = userInput.slice(0, 4);
 	if(typeof userInput !== 'string') {return "Invalid Input. Please input a string."}
 
 	// run 2 checks (one for vowels, then one for consonant clusters)
 	// then 2 loops depending on match: one for the vowels against the string, one for consonant clusters against the string?
 	// maybe use array.match()  ?
 	
-	if (userInput[0] == vowels) {
-		return userInput.slice[1] + userInput[0].toLowerCase() + "ay";
-	}
+
+	
+
+		for (let i = 0; i < vowels.length; i++) {
+			let thisLetter = vowels[i];
+			if (userInput[0] == thisLetter) {
+			return userInput + "ay";
+			}
+		}
+		
+		for (let j = 0; j < twoCharConsonantClusters.length; j++) {
+			let theseTwo = twoCharConsonantClusters[j];
+			if (firstTwo == theseTwo) {
+				return userInput.slice(2) + firstTwo + "ay";
+			}
+		}
+		
+		for (let k = 0; k < threeCharConsonantClusters.length; k++) {
+			let theseThree = threeCharConsonantClusters[k];
+			if (firstThree == theseThree) {
+				return userInput.slice(3) + firstThree + "ay";
+			}
+		}
 }
 
 
