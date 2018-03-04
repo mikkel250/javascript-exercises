84e37ae7c4bc4aafc15faa5a35e5df42d8de1d17
Mikkel Ridley <mikkel250@gmail.com>
2018-03-04 11:07:17 -0800

    save before changes 3-4-2018

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-var fibonacci = function() {
-
+var fibonacci = function(number) {
+// formula to approximate if n > 0.
+// F(n) = round(Phi^n / math.sqrt(5))
+var phi = (Math.sqrt(5) + 1) / 2;
+	if (number > 0) {
+		return Math.round(phi ** number / Math.sqrt(5));    // the ** is exponent
+	} else {
+		return "OOPS"
+	}
 }
 
 module.exports = fibonacci
