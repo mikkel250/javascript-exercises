03f7fdfa5b931ff9d2f078168897217e430690ac
mikkel250 <mikkel250@gmail.com>
2019-01-23 16:19:09 -0800

    format caesar - prettied up

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,45 +1,41 @@
 var caesar = function(string, shift) {
-	return string
-								.split("")
-								.map(char => shiftCharacter(char, shift) )
-								.join("");	
-}
+  return string
+    .split("")
+    .map(char => shiftCharacter(char, shift))
+    .join("");
+};
 
 // Check the case of the letter
-function upperOrLowerCase (char) {
-	if (char.charCodeAt() < 97) {
-		return 65;
-	} else {
-		return 97;
-	}
+function upperOrLowerCase(char) {
+  if (char.charCodeAt() < 97) {
+    return 65;
+  } else {
+    return 97;
+  }
 }
 
 // Get the character code
-function getCharCode (char) {
-	return char.charCodeAt();
+function getCharCode(char) {
+  return char.charCodeAt();
 }
 
-function getNewCode (code, shift, charCase) {
-	let shiftAmountBeyondLetterA = code + shift - charCase;
-	let finalCode = (shiftAmountBeyondLetterA % 26 + 26) % 26 + charCase;
-	return finalCode;
+function getNewCode(code, shift, charCase) {
+  let shiftAmountBeyondLetterA = code + shift - charCase;
+  let finalCode = (((shiftAmountBeyondLetterA % 26) + 26) % 26) + charCase;
+  return finalCode;
 }
 
-
-// Shift the character 
-function shiftCharacter (char, shift) {
-	let code = getCharCode(char);  // get the ascii code for the character
-	let charCase = upperOrLowerCase(char); 	// determine upper or lower case, and set 'a' or 'A' as the starting point for the shift
-	if ( (code > 64 && code < 91) || (code > 96 && code < 123) ) {    // if A-Z or a-z
-	let shiftedChar = getNewCode(code, shift, charCase);  // wrap around alphabet if necessary (the % gets the amount of shift, starting from the letter 'a', and charCase starts the count from 'a'). If the shift is past the last letter, it starts the count from 'a'. If it doesn't wrap around, you're getting the same number you would get if you added the charCode and shift together anyway. 
-	return String.fromCharCode(shiftedChar);	// return the string corresponding to the number code
-	} else {
-		return char; 	// otherwise, just return the character without modifying it
-	}
-
+// Shift the character
+function shiftCharacter(char, shift) {
+  let code = getCharCode(char); // get the ascii code for the character
+  let charCase = upperOrLowerCase(char); // determine upper or lower case, and set 'a' or 'A' as the starting point for the shift
+  if ((code > 64 && code < 91) || (code > 96 && code < 123)) {
+    // if A-Z or a-z
+    let shiftedChar = getNewCode(code, shift, charCase); // wrap around alphabet if necessary (the % gets the amount of shift, starting from the letter 'a', and charCase starts the count from 'a'). If the shift is past the last letter, it starts the count from 'a'. If it doesn't wrap around, you're getting the same number you would get if you added the charCode and shift together anyway.
+    return String.fromCharCode(shiftedChar); // return the string corresponding to the number code
+  } else {
+    return char; // otherwise, just return the character without modifying it
+  }
 }
 
-
-module.exports = caesar
-
-
+module.exports = caesar;
